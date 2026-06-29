6f5a6d054091955c3f7ca49d19fc77531287a84f
naabiiill <nabilmahmud.tech@gmail.com>
2026-06-29 13:38:47 +0600

    building a new calculator with getting user input

--- typecasting.cs
+++ typecasting.cs
@@ -254,7 +254,34 @@ static int GetMax(int num1, int num2, int num3)
        
     return result;    */
 
+ //builing new smart calculator
 
+ /*Console.Write("enter a number: ");
+ double num1=Convert.ToDouble (Console.ReadLine());
+ Console.Write("enter operator");
+ string op= Console.ReadLine();
+ Console.Write("enter a number: ");
+ double num2 = Convert.ToDouble(Console.ReadLine());
+
+ if (op== "+")
+ {
+     Console.WriteLine(num1+num2);
+ }else if ( op== "-")
+ {
+     Console.WriteLine(num1 -num2);
+ }
+ else if (op == "*")
+ {
+     Console.WriteLine(num1 * num2);
+ }
+ else if (op == "/")
+ {
+     Console.WriteLine(num1 / num2);
+ }
+ else
+ {
+     Console.WriteLine("invalid operator");
+ }    */
             
         }
     }
